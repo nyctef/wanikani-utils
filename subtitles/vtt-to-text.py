5ebe70e126cf5b6c17a63a4068e8be51a3402b2d
Nyctef <nyctef@nyctef.com>
2022-09-28 17:51:42 +0100

    quick script to remove metadata from .vtt subtitles

---
+++ subtitles/vtt-to-text.py
@@ -0,0 +1,13 @@
+import webvtt
+import sys
+
+if len(sys.argv) <= 1:
+    print("Usage: [path/to/subtitles.vtt]")
+    sys.exit(1)
+
+path = sys.argv[1];
+
+for caption in webvtt.read(path):
+    # bug (?) in webvtt: not handling this xml escape for us
+    text = caption.text.replace("&lrm;", "")
+    print(text)
