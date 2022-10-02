0de5465c3a3c05f8c9d12837b9856ff20a2a403a
Nyctef <nyctef@nyctef.com>
2022-10-02 14:54:01 +0100

    include radical in skip lookup

--- kanjidic/skip-to-pg.py
+++ kanjidic/skip-to-pg.py
@@ -10,16 +10,18 @@ if len(sys.argv) <= 1:
     print("Usage: [path/to/kanjidic2.xml.gz]")
     sys.exit(1)
 
-skip_to_char = defaultdict(list)
-char_to_skip = {}
+skip_rad_to_char = defaultdict(list)
 
 with gzip.open(sys.argv[1]) as fin:
     tree = ET.parse(fin).getroot()
     for charElement in tree.findall('character'):
         char = charElement.find('literal').text
         skip = charElement.find(".//q_code[@qc_type='skip']").text
-        char_to_skip[char] = skip
-        skip_to_char[skip].append(char)
+        radicalNum = charElement.find(".//rad_value[@rad_type='classical']").text
+        # 0x2F00 is the start of the radical block in unicode: https://en.wikipedia.org/wiki/Kangxi_Radicals_(Unicode_block)
+        # subtract 1 since the radical number is 1-indexed but the unicode block is 0-indexed
+        radical = chr(0x2F00 - 1 + int(radicalNum))
+        skip_rad_to_char[skip + " " + radical].append(char)
 
-for entry in sorted(skip_to_char.items(), key=lambda x: len(x[1])):
+for entry in sorted(skip_rad_to_char.items(), key=lambda x: len(x[1])):
     print(entry[0] + ": " + str(entry[1]))
